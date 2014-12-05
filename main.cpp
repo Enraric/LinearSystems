29eb9a3b613be4f4595747eb32b7f4b64abec492
NinjaSnowman66 <alex.wilson667@gmail.com>
2014-12-05 11:34:13 -0500

    Basically nothing

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+// Alex's job: Math Things
+// Harry's job: parsing
+// Wilson's job: File stuff + misc...
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "time.h"
+
+int main(){
+	
+}
