682512c02a496273addd1accae4bf58f9258f78d
Enraric <colak148@gmail.com>
2014-12-05 11:33:53 -0500

    added structs

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
 
+
+struct Line {
+       int a;
+       int b;
+       int e;
+};
+
+struct System {
+       Line one
+       Line two
+};
